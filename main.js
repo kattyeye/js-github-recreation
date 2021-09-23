5e263973c57b6ffbcd3e9b661aa64a27b478eabe
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-22 22:04:05 -0400

    Adding four fetches in console only

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
 (function () {
     'use strict';
 
-
-
-    fetch(`https://api.github.com/users/kattyeye/repos`)
+fetch(`https://api.github.com/users/kattyeye`)
     .then((response) => response.json())
     .then((data) => console.log(data))
         .then((data) => generateHTML(data));
 
+
     const generateHTML = (data) => {
-    const source = document.querySelector("#repos-template").innerHTML;
+    const source = document.querySelector("#git-bio-template").innerHTML;
     const template = Handlebars.compile(source);
-    const context = data;
-    const html = template(context);
+    const html = template(data);
+    document.querySelector(".container").innerHTML = html;
+    };
+
+    const generateRepoStuff = (data) => {
+    const source = document.querySelector("#git-repo-template").innerHTML;
+    const template = Handlebars.compile(source);
+        const html = template({ data });
+        console.log(data)
     document.querySelector(".container").innerHTML = html;
     };
 
 
 
-    // fetch(`https://api.github.com/users/kattyeye/repos`)
-    // .then((response) => response.json())
-    // .then((data) => console.log(data))
-    //     .then((data) => generateHTML(data));
-
-    // const generateHTML = (data) => {
-    // const source = document.querySelector("#repos-template").innerHTML;
-    // const template = Handlebars.compile(source);
-    // const context = data;
-    // const html = template(context);
-    // document.querySelector(".container").innerHTML = html;
-    // };
+    fetch(`https://api.github.com/users/kattyeye/repos`)
+        .then(response => response.json())
+        .then((data) => console.log(data))
+            .then((data) => generateRepoStuff(data));
 
 
+    fetch(`https://api.github.com/users/kattyeye/followers`)
+        .then(response => response.json())
+       .then((data) => console.log(data))
+
+
+    fetch(`https://api.github.com/users/kattyeye/following`)
+        .then(response => response.json())
+       .then((data) => console.log(data))
 
 }) ();
