594eeb8dd086e07f43677b1e634400286076c121
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-24 11:57:49 -0400

    Adding partial data display, works some of the time

--- main.js
+++ main.js
@@ -1,42 +1,50 @@
 (function () {
     'use strict';
 
-fetch(`https://api.github.com/users/kattyeye`)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-        .then((data) => generateHTML(data));
-
-
-    const generateHTML = (data) => {
-    const source = document.querySelector("#git-bio-template").innerHTML;
-    const template = Handlebars.compile(source);
-    const html = template(data);
-    document.querySelector(".container").innerHTML = html;
-    };
-
-    const generateRepoStuff = (data) => {
-    const source = document.querySelector("#git-repo-template").innerHTML;
-    const template = Handlebars.compile(source);
-        const html = template({ data });
-        console.log(data)
-    document.querySelector(".container").innerHTML = html;
-    };
 
 
 
-    fetch(`https://api.github.com/users/kattyeye/repos`)
+     const userFetch = fetch(`https://api.github.com/users/kattyeye`)
+            .then((response) => response.json())
+    .then((data) => generateHTML(data))
+
+
+
+
+    const repoFetch = fetch(`https://api.github.com/users/kattyeye/repos`)
+        .then(response => response.json())
+            .then((data) => generateRepoStuff(data))
+
+
+    const followersFetch = fetch(`https://api.github.com/users/kattyeye/followers`)
         .then(response => response.json())
         .then((data) => console.log(data))
-            .then((data) => generateRepoStuff(data));
 
 
-    fetch(`https://api.github.com/users/kattyeye/followers`)
+   const followingFetch = fetch(`https://api.github.com/users/kattyeye/following`)
         .then(response => response.json())
-       .then((data) => console.log(data))
+        .then((data) => console.log(data))
 
+    const allData = Promise.all([
+        userFetch, repoFetch, followersFetch, followingFetch,
+    ])
+    allData.then((data) => {
+        const generateHTML = (data) => {
+            const source = document.querySelector("#git-bio-template").innerHTML;
+            const template = Handlebars.compile(source);
+            const html = template(data);
+            document.querySelector(".container").innerHTML = html;
+        };
 
-    fetch(`https://api.github.com/users/kattyeye/following`)
-        .then(response => response.json())
-       .then((data) => console.log(data))
+    const generateRepoStuff = (data) => {
+        // console.log(document.querySelector("#git-bio-template").innerHTML)
+        const source = document.querySelector("#git-repo-template").innerHTML;
+        const template = Handlebars.compile(source);
+        const html = template(data);
+        console.log(data)
+        document.querySelector(".container").innerHTML = html;
+    };
+    })
+
 
 }) ();
